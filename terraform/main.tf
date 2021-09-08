b7dba6c966d00a2da8f0384b2c8a09506905431e
Jeff Haineault <jhaineault@Jeffs-MacBook-Pro.local>
2021-09-08 12:10:55 -0700

    Added 2nd container to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,30 @@
 provider "aws" {
-	region = "us-west-1"
+  region = "us-west-1"
 }
 
 variable "name" {
   description = "Name the instance on deploy"
 }
 
-resource "aws_instance' "devops_01" {
-	ami = "ami-04b9e92b5572fa0d1"
-	instace_type = "t2.micro"
+variable "name2" {
+  description = "Name 2nd instance on deploy"
+}
+
+resource "aws_instance" "devops_01" {
+  ami = "ami-0fdf8b5989f22a4e0"
+	instance_type = "t2.micro"
+  key_name = "portfolio-2021"
+
+  tags = {
+    Name = "${var.name}"
+  }
+}
+resource "aws_instance" "devops_02" {
+  ami = "ami-0fdf8b5989f22a4e0"
+	instance_type = "t2.micro"
 	key_name = "portfolio-2021"
 
 	tags = {
-		Name = "${var.name}"
+	  Name = "${var.name2}"
 	}
 }
